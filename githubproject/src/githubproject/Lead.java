0292c0ed884233c74a2560ec9b787fcd621c32a6
likit <likit@Likhith-HP>
2022-12-22 09:45:08 +0530

    do your tasks

---
+++ githubproject/src/githubproject/Lead.java
@@ -0,0 +1,10 @@
+package githubproject;
+
+public class Lead {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+             System.out.println("this is the project");
+	}
+
+}
